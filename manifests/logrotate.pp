10688c8395cf5ecb4c2c780f9ffe24215a465f9c
Eugene <47208594+zhituha@users.noreply.github.com>
2024-08-26 22:30:18 +0300

    Update logrotate.pp

--- manifests/logrotate.pp
+++ manifests/logrotate.pp
@@ -1,3 +1,19 @@
+# Configure a logrotate.
+# @param logrotate_dir Base dir for logrotate configs in your system. Default: '/etc/logrotate.d/'
+# @param logrotate_conf Filename for an ocserv's config. Default: 'ocserv.conf'
+# @param log_dir Dir where to store logs. Default: '/var/log'
+# @param log_file Filename for an ocserv's logfile. Default: 'ocserv.log'
+# @param logs_count How many logs you'd like to keep on a disk. Default: 7
+# @param period Period of rotating.  Default: 'daily'
+# @param compress Either compress logs or not. Default: true
+# @param delaycompress Postpone compressing current active logfile to the next rotation. Default: true
+# @param missingok Do not throw an error if some logfile is missing. Just go to the next one. Default: true
+# @param ifempty Either rotate or not empty logs. Default: true
+# @param copytruncate Truncate the original log after creating a copy instead of moving and creating a new one. Default: true
+# @param create Create new logfile immediately after rotating. Default: true
+# @param create_mode Set filemode, ownar, and group for the new logfile.
+# @param size Rotate logfile only if it grow bigger than specified size.
+
 class ocserv::logrotate (
 
   String[1]           $logrotate_dir   = '/etc/logrotate.d/',
